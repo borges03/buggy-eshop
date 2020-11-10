8ffb1bd1060845d1d659ea90ba28e7b6032446ff
Genesis Borges <genesis.borges@upr.edu>
2020-11-10 19:37:16 -0400

    Uploading previous files

--- Javascript/style.js
+++ Javascript/style.js
@@ -16,13 +16,33 @@ for (i = 0; i < acc.length; i++) {
   
 }
 // Controls the quantity of products selected by user
-var Count = 0, j;
+$('.minus-btn').on('click', function(e) {
+  e.preventDefault();
+  var $this = $(this);
+  var $input = $this.closest('div').find('input');
+  var value = parseInt($input.val());
 
-function funcOne(){
-   Count-- < 1 ? Count = 0 : ''; 
-   document.getElementById("number").innerHTML=Count;
-}
-function funcTwo(){
-   Count++;               
-   document.getElementById("number").innerHTML=Count;
-}
+  if (value > 1) {
+      value = value - 1;
+  } else {
+      value = 0;
+  }
+
+$input.val(value);
+
+});
+
+$('.plus-btn').on('click', function(e) {
+  e.preventDefault();
+  var $this = $(this);
+  var $input = $this.closest('div').find('input');
+  var value = parseInt($input.val());
+
+  if (value < 100) {
+      value = value + 1;
+  } else {
+      value =100;
+  }
+
+  $input.val(value);
+});
